49ecbd551a559c4b64675fa615308ae65573a256
YYanin <yaroslav1304@gmail.com>
2024-08-25 21:13:53 -0700

    removed some commented out code, added a function to take input, added a function to clear the screen based on what os the user is using, and introduced some ascii art for working and for resting

--- timer.py
+++ timer.py
@@ -1,29 +1,90 @@
 ###Code for the timer
 import time
+
+##For the timer
 import sys
+
+##This way we can clear the old print statements
+import os
+
 
 
 def main():
-    print("Pomodoro Timer \n")
 
-    print("Enter the work time: \n")
+    dogWorking="""
+    
+             .--~~,__
+:-....,-------`~~'._.'
+ `-,,,  ,_      ;'~U'
+  _,-' ,'`-__; '--.
+ (_/'~~      ''''(;
+    
+    """
+    
+    clearScreen()
+    
+    dogResting="""
+    
+  __    __
+o-''))_____\\
+"--__/ * * * )
+c_c__/-c____/
+    
+    """
+    
+    clearScreen()    
 
-    ##Grab the values and convert them to an int
-    x=int(input("Enter the time for pure concentration in minutes: "))
+
+    x=getInput(("Enter the time for pure concentration in minutes: "))
     
-    y=int(input("Enter the time for rest and relaxation: "))
+    y=getInput(("Enter the time for rest and relaxation: "))
     
-    z=int(input("Enter how many sets of work you want to do: "))
+    z=getInput(("Enter how many sets of work you want to do: "))
     
-    countDown(x,y,z)
+    
+    #Create the main loop
+    while z>=0:
+        ##Run the work countdown
+        countDown(x,dogWorking)
+        ##Now we can run the rest period
+        countDown(y,dogResting)
+        
+        z=z-1    
+    
+
+    
+    ## Now we can add the end screen
+    
+    
+    
+    
     
     return 0
 
-def countDown(work,rest,set):
+
+
+def clearScreen():
+    # clear the console screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def getInput(prompt):
+
+    ##Grab the values and convert them to int
+    x=int(input(prompt))
+    
+    clearScreen()
+    
+    return x
+
+
+
+def countDown(count,art):
     
     ##Convert Min to Sec
-    seconds=work*60
+    seconds=count*60
     
+    ##Print the art
+    print(art, end="\r")
     
     while(seconds>=0):
         
@@ -37,22 +98,29 @@ def countDown(work,rest,set):
         
         ##Show double zeros if there are no seconds left for visual appeal
         if secondsLeft==0:
-            sys.stdout.write('\r{min}:00'.format(min=int(minutes)))
+            sys.stdout.write('\r        {min}:00 '.format(min=int(minutes)))
             time.sleep(1)
-            seconds=seconds-1
-        ##elif secondsLeft==0 and minutes==0:
-            ##This elif will be triggered when the timer reaches zero
-            
+            seconds=seconds-1  
         else:
             ##Write an updating countdown 
             ##The space after the seconds value allows us to overwrite the second zero in the seconds tab as stdout doesn't overwrite the 
-            sys.stdout.write('\r{min}:{sec} '.format(min=int(minutes),sec=int(secondsLeft)))
+            sys.stdout.write('\r        {min}:{sec} '.format(min=int(minutes),sec=int(secondsLeft)))
             time.sleep(1)
             seconds=seconds-1
         
-    
+    clearScreen()
     return 0
+
+def end():
+    clearScreen()
     
+    x="""
+    
+    
+    
+    
+    """
+    #print()
 
 
 if __name__== '__main__':
